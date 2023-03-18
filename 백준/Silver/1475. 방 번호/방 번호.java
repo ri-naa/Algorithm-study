d598d7882577fb530d7c28e32835be284ce1ee6a
Rina Kim <121746871+ri-naa@users.noreply.github.com>
2023-03-18 14:37:51 +0900

    [Silver V] Title: 방 번호, Time: 208 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,15 +1,11 @@
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
+		Scanner scanner = new Scanner(System.in);		
 		String s = scanner.next();
 		
 		int num[] = new int[9];
-		
 		for(int i=0; i<s.length(); i++) {
 			int a = s.charAt(i)-'0';
 			if(a==6 || a==9) {
@@ -20,7 +16,6 @@ public class Main {
 		}
 		
 		num[6] = (num[6]+1)/2;
-		
 		int max=num[0];
 		for(int i=0; i<9; i++) {
 			if(max<num[i]) max=num[i];
